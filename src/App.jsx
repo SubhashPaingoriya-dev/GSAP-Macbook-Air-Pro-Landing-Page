d94be310b737b18e1c9e4fc4d4e6589f629f9ba9
Subhash Paingoriya <subhashpaingoriya@Subhashs-MacBook-Air.local>
2026-03-15 18:31:38 +0530

    Implemented Showcase Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero.jsx'
 import ProductViewer from './components/ProductViewer.jsx'
 import gsap from 'gsap';
 import { ScrollTrigger , SplitText } from 'gsap/all';
+import Showcase from './components/Showcase.jsx';
 gsap.registerPlugin(ScrollTrigger)
 gsap.registerPlugin(SplitText) 
 
@@ -13,6 +14,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <ProductViewer />
+        <Showcase />
     </div>
   )
 }
